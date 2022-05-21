dc959536e68086a8d59c6947467e85d2bc6676cc
Michael Goodnow <mmgoodnow@gmail.com>
2022-05-20 23:19:32 -0400

    Set up docusaurus config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -6,18 +6,13 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-	title: "My Site",
-	tagline: "Dinosaurs are cool",
-	url: "https://your-docusaurus-test-site.com",
+	title: "cross-seed",
+	tagline: "Fully automatic cross-seeding, no false positives",
+	url: "https://cross-seed.org",
 	baseUrl: "/",
 	onBrokenLinks: "throw",
 	onBrokenMarkdownLinks: "warn",
 	favicon: "img/favicon.ico",
-
-	// GitHub pages deployment config.
-	// If you aren't using GitHub pages, you don't need these.
-	organizationName: "facebook", // Usually your GitHub org/user name.
-	projectName: "docusaurus", // Usually your repo name.
 
 	// Even if you don't use internalization, you can use this field to set useful
 	// metadata like html lang. For example, if your site is Chinese, you may want
@@ -37,14 +32,7 @@ const config = {
 					// Please change this to your repo.
 					// Remove this to remove the "edit this page" links.
 					editUrl:
-						"https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-				},
-				blog: {
-					showReadingTime: true,
-					// Please change this to your repo.
-					// Remove this to remove the "edit this page" links.
-					editUrl:
-						"https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+						"https://github.com/mmgoodnow/cross-seed/website/docs/",
 				},
 				theme: {
 					customCss: require.resolve("./src/css/custom.css"),
@@ -57,7 +45,7 @@ const config = {
 		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
 		({
 			navbar: {
-				title: "My Site",
+				title: "cross-seed",
 				logo: {
 					alt: "My Site Logo",
 					src: "img/logo.svg",
@@ -70,7 +58,7 @@ const config = {
 						label: "Tutorial",
 					},
 					{
-						href: "https://github.com/facebook/docusaurus",
+						href: "https://github.com/mmgoodnow/cross-seed",
 						label: "GitHub",
 						position: "right",
 					},
@@ -92,12 +80,8 @@ const config = {
 						title: "Community",
 						items: [
 							{
-								label: "Stack Overflow",
-								href: "https://stackoverflow.com/questions/tagged/docusaurus",
-							},
-							{
 								label: "Discord",
-								href: "https://discordapp.com/invite/docusaurus",
+								href: "https://discord.gg/jpbUFzS5Wb",
 							},
 							{
 								label: "Twitter",
@@ -110,12 +94,12 @@ const config = {
 						items: [
 							{
 								label: "GitHub",
-								href: "https://github.com/facebook/docusaurus",
+								href: "https://github.com/mmgoodnow/cross-seed",
 							},
 						],
 					},
 				],
-				copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+				copyright: `Copyright © 2020-${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
 			},
 			prism: {
 				theme: lightCodeTheme,
