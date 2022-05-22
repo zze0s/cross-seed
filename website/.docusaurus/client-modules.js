d2b52c20b0824f5f66b6e367daefe4e0edbc75c3
Michael Goodnow <mmgoodnow@gmail.com>
2022-05-22 14:55:26 -0400

    --wip-- [skip ci]

---
+++ website/.docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require('/Users/mmgoodnow/Documents/Projects/cross-seed/website/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/mmgoodnow/Documents/Projects/cross-seed/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/mmgoodnow/Documents/Projects/cross-seed/website/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
+  require('/Users/mmgoodnow/Documents/Projects/cross-seed/website/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/mmgoodnow/Documents/Projects/cross-seed/website/src/css/custom.css'),
+];
